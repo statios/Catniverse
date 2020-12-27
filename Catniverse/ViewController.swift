0512a402c95cf1042ce716d0d887c77b59bf590a
statios <statios@kakao.com>
2020-12-28 01:07:36 +0900

    Add iOS project

---
+++ Catniverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Catniverse
+//
+//  Created by KIHYUN SO on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
